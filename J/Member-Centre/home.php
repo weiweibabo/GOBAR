0d6dc8069395bc78f63e50bcbd34e29ebb3764fa
Johnny-TW <a0988298334@gmail.com>
2020-11-18 18:25:11 +0800

    建立個人資料夾

---
+++ J/Member-Centre/home.php
@@ -0,0 +1,10 @@
+<?php include __DIR__ . '/part/config.php'; ?>
+<?php include __DIR__ . '/part/html-header.php'; ?>
+<?php include __DIR__ . '/part/navbar.php'; ?>
+
+<div class="container">
+    <h2>GoBar</h2>
+</div>
+
+<?php include __DIR__ . '/part/scripts.php'; ?>
+<?php include __DIR__ . '/part/html-footer.php'; ?>
---
+++ J/Member-Centre/part/scripts.php
@@ -0,0 +1,2 @@
+<script src="<?= WEB_ROOT ?>lib/jquery-3.5.1.js"></script>
+<script src="<?= WEB_ROOT ?>bootstrap/js/bootstrap.bundle.js"></script>
