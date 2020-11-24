0be52a48af39f298a4f55f1f30dabf15af453655
weiweibabo <weiweibabo@gmail.com>
2020-11-25 01:07:45 +0800

    調整檢查判定

--- W/js/order.js
+++ W/js/order.js
@@ -6,60 +6,50 @@ const name = document.getElementById('name');
 const phoneNumber = document.getElementById('phonenumber');
 const email = document.getElementById('email');
 const errormsg = document.getElementById('errormsg');
-const sendWeb = document.getElementById('send-web');
-const sendPhone = document.getElementById('send-phone');
+const send = document.getElementById('send');
 const err1 = document.getElementById('err1');
 const err2 = document.getElementById('err2');
 const err3 = document.getElementById('err3');
 
-const phoneId = document.getElementById('phone');
+send.addEventListener('click', () => {
+  const { userAgent } = navigator;
 
-sendWeb.addEventListener('click', () => {
-  if (
-    persons.value === '請選擇'
-      || time.value === '請選擇'
-      || name.value === ''
-      || phoneNumber.value === ''
-      || email.value === ''
+  if ((userAgent.match('Chrome')
+      || userAgent.match('Safari')
+      || userAgent.match('Firefox')
+      || userAgent.match('Opera'))
+      && !userAgent.match('Mobile')
   ) {
-    errormsg.innerHTML = '<i class="fas fa-exclamation-circle"></i> 資料填寫不完整';
-    sendWeb.classList.add('next-btn2');
-  } else {
-    errormsg.innerHTML = '';
-    sendWeb.classList.remove('next-btn2');
-  }
-  if (name.value === '') {
-    err1.innerHTML = 'Error <i class="fas fa-exclamation-circle"></i>';
-  } else {
-    err1.innerHTML = '';
-  }
-  if (phoneNumber.value === '') {
-    err2.innerHTML = 'Error <i class="fas fa-exclamation-circle"></i>';
-  } else {
-    err2.innerHTML = '';
-  }
-  if (email.value === '') {
-    err3.innerHTML = 'Error <i class="fas fa-exclamation-circle"></i>';
-  } else {
-    err3.innerHTML = '';
-  }
-});
-
-sendPhone.addEventListener('click', () => {
-  if (
-    selectDate.value === ''
+    if (
+      persons.value === '請選擇'
+        || time.value === '請選擇'
+        || name.value === ''
+        || phoneNumber.value === ''
+        || email.value === ''
+    ) {
+      errormsg.innerHTML = '<i class="fas fa-exclamation-circle"></i> 資料填寫不完整';
+      send.classList.add('next-btn2');
+    } else {
+      errormsg.innerHTML = '';
+      send.classList.remove('next-btn2');
+    }
+  } else if (userAgent.match('Mobile')) {
+    if (
+      selectDate.value === ''
       || persons.value === '請選擇'
       || time.value === '請選擇'
       || name.value === ''
       || phoneNumber.value === ''
       || email.value === ''
-  ) {
-    errormsg.innerHTML = '<i class="fas fa-exclamation-circle"></i> 資料填寫不完整';
-    sendPhone.classList.add('next-btn2');
-  } else {
-    errormsg.innerHTML = '';
-    sendPhone.classList.remove('next-btn2');
+    ) {
+      errormsg.innerHTML = '<i class="fas fa-exclamation-circle"></i> 資料填寫不完整';
+      send.classList.add('next-btn2');
+    } else {
+      errormsg.innerHTML = '';
+      send.classList.remove('next-btn2');
+    }
   }
+
   if (name.value === '') {
     err1.innerHTML = 'Error <i class="fas fa-exclamation-circle"></i>';
   } else {
