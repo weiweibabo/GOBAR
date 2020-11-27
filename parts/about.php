8b5d707b567b864ee0d63d3425b3ed9035b8d343
f45641 <f45641@gmail.com>
2020-11-27 13:59:17 +0800

    about-php完成

---
+++ parts/about.php
@@ -0,0 +1,38 @@
+<link rel="stylesheet" href="<?= WEB_ROOT ?>css/about.css">
+
+<body>
+    <div class="about">
+        <div class="container-fluid">
+            <div class="aboutrow">
+                <div class="footerlogo">
+                    <img src="./img/logowhite.svg" alt="">
+                </div>
+                <ul class="aboutitem">
+                    <il class="contact">聯絡我們</il>
+                    <il class="abgobar">關於GoBar</il>
+                    <il class="cooper">企業合作</il>
+                    <il class="server">服務條款</il>
+                    <il class="privacy">隱私權聲明</il>
+                </ul>
+                <ui class="community">
+                    <il><img src="./img/fb.svg" alt=""></il>
+                    <il><img src="./img/ig.svg" alt=""></il>
+                    <il><img src="./img/twiter.svg" alt=""></il>
+                </ui>
+            </div>
+            <h6 class="copyright">© GoBar All Rights Resevered</h6>
+        </div>
+    </div>
+    <div class="ban">
+        <div class="container-fluid">
+            <div class="banrow">
+                <h3>禁止酒駕</h3>
+                <div class="banimg">
+                    <img src="./img/banimg.png" alt="">
+                </div>
+                <h3>未滿18歲禁止飲酒</h3>
+            </div>
+        </div>
+    </div>
+
+</body>
