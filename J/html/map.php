40c605e2dd74f269d0a962a43d4ec3bcfb4ae256
Johnny-TW <a0988298334@gmail.com>
2020-11-29 23:31:30 +0800

    11-29-check

---
+++ J/html/map.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <script
+    src="https://maps.googleapis.com/maps/api/js?key=API_KEY&map_ids=MAP_ID&callback=initMap">
+    </script>
+    
+</body>
+
+</html>
+
+
+
