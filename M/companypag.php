859ada683ab807bfd6b1c6a06e9a044d9cc0acc3
f45641 <f45641@gmail.com>
2020-12-01 20:34:42 +0800

    12012034

--- M/companypag.php
+++ M/companypag.php
@@ -62,6 +62,44 @@
         </div>
     </section>
     <section>
+        <div class=" store">
+            <div class="store-name d-flex">
+                <input class="inputstore-name" placeholder="✎輸入店家名稱"></input>
+            </div>
+            <form class="titlepc" action="" method="post" enctype="multipart/form-data" onclick="field2.click()">
+                <input type="file" class="inputbigpc bigpc2" name="avatar" accept="image/*" onchange="previewFile2()"></input>
+                <img id="inputbigimg2" src="<?= WEB_ROOT ?>img/addimg.svg" alt="Image preview...">
+
+            </form>
+            <div class="information-bar position-absolute d-block w-100">
+                <div class="information">
+                    <div class="star">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="295" height="33" viewBox="0 0 295 33">
+                            <g id="Group_804" data-name="Group 804" transform="translate(-317.5 -916.06)">
+                                <path id="Polygon_1" data-name="Polygon 1" d="M16.616,1.674a1,1,0,0,1,1.768,0l4.485,8.49a1,1,0,0,0,.718.519l9.517,1.6a1,1,0,0,1,.546,1.688l-6.738,6.832a1,1,0,0,0-.277.849l1.4,9.462a1,1,0,0,1-1.43,1.044L17.941,27.9a1,1,0,0,0-.882,0L8.394,32.16a1,1,0,0,1-1.43-1.044l1.4-9.462a1,1,0,0,0-.277-.849L1.35,13.974A1,1,0,0,1,1.9,12.286l9.517-1.6a1,1,0,0,0,.718-.519Z" transform="translate(317.5 916.06)" fill="gold" />
+                                <path id="Polygon_1-2" data-name="Polygon 1" d="M16.616,1.674a1,1,0,0,1,1.768,0l4.485,8.49a1,1,0,0,0,.718.519l9.517,1.6a1,1,0,0,1,.546,1.688l-6.738,6.832a1,1,0,0,0-.277.849l1.4,9.462a1,1,0,0,1-1.43,1.044L17.941,27.9a1,1,0,0,0-.882,0L8.394,32.16a1,1,0,0,1-1.43-1.044l1.4-9.462a1,1,0,0,0-.277-.849L1.35,13.974A1,1,0,0,1,1.9,12.286l9.517-1.6a1,1,0,0,0,.718-.519Z" transform="translate(382.5 916.06)" fill="gold" />
+                                <path id="Polygon_1-3" data-name="Polygon 1" d="M16.616,1.674a1,1,0,0,1,1.768,0l4.485,8.49a1,1,0,0,0,.718.519l9.517,1.6a1,1,0,0,1,.546,1.688l-6.738,6.832a1,1,0,0,0-.277.849l1.4,9.462a1,1,0,0,1-1.43,1.044L17.941,27.9a1,1,0,0,0-.882,0L8.394,32.16a1,1,0,0,1-1.43-1.044l1.4-9.462a1,1,0,0,0-.277-.849L1.35,13.974A1,1,0,0,1,1.9,12.286l9.517-1.6a1,1,0,0,0,.718-.519Z" transform="translate(447.5 916.06)" fill="gold" />
+                                <path id="Path_6156" data-name="Path 6156" d="M16.616,1.674a1,1,0,0,1,1.768,0l4.485,8.49a1,1,0,0,0,.718.519l9.517,1.6a1,1,0,0,1,.546,1.688l-6.738,6.832a1,1,0,0,0-.277.849l1.4,9.462a1,1,0,0,1-1.43,1.044L17.941,27.9a1,1,0,0,0-.882,0L8.394,32.16a1,1,0,0,1-1.43-1.044l1.4-9.462a1,1,0,0,0-.277-.849L1.35,13.974A1,1,0,0,1,1.9,12.286l9.517-1.6a1,1,0,0,0,.718-.519Z" transform="translate(512.5 916.06)" fill="gold" />
+                                <path id="Polygon_1-4" data-name="Polygon 1" d="M16.616,1.674a1,1,0,0,1,1.768,0l4.485,8.49a1,1,0,0,0,.718.519l9.517,1.6a1,1,0,0,1,.546,1.688l-6.738,6.832a1,1,0,0,0-.277.849l1.4,9.462a1,1,0,0,1-1.43,1.044L17.941,27.9a1,1,0,0,0-.882,0L8.394,32.16a1,1,0,0,1-1.43-1.044l1.4-9.462a1,1,0,0,0-.277-.849L1.35,13.974A1,1,0,0,1,1.9,12.286l9.517-1.6a1,1,0,0,0,.718-.519Z" transform="translate(577.5 916.06)" fill="gold" />
+                            </g>
+                        </svg>
+
+                    </div>
+                    <div class="d-flex">
+                        <div class="address">
+                            <input class="inputaddress" placeholder="✎輸入店家地址"></input>
+                            <input class="inputphon" placeholder="✎輸入店家電話"></input>
+                        </div>
+                        <div class="business-hour">
+                            <input class="inputhour" placeholder="✎輸入營業時間"></input>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <section>
         <div class="container-fluid">
             <div class="news">
                 <div class="news-title-wrap d-flex">
@@ -248,7 +286,7 @@
         </div>
 
     </section>
-    <section class="line03">
+    <!-- <section class="line03">
         <div class="intro container-fluid">
             <h3>特殊餐點推薦</h3>
             <div class="food-intro d-flex ">
@@ -281,31 +319,31 @@
                     <path id="Path_79" data-name="Path 79" d="M185.09,15.68,185.1,13c-.042-.039-.086-.075-.126-.115a6.291,6.291,0,1,1,9-.125l.006,2.774a8.444,8.444,0,1,0-8.892.144Z" transform="translate(-81.158)" fill="#fff" />
                     <path id="Path_80" data-name="Path 80" d="M117.272,85.087a44.535,44.535,0,0,1-3.4,3.536,1.707,1.707,0,0,1-2.887-1.236l-.041-17.746v.323a2.559,2.559,0,0,0-5.117.072l-.042,12.86L95.363,85.59A6.071,6.071,0,0,0,92,93.118l2.605,9.772v5.086h16.906V102.89s8.737-13.182,9.637-14.458C122.76,86.158,119.948,82.142,117.272,85.087Z" transform="translate(0 -61.329)" fill="#fff" />
                 </g>
-            </svg></div>
+            </svg></div> -->
 
-        <h2>CLICK</h2>
-        <div class="form-wrapper">
-            <div class="sp-time">
-                <svg xmlns="http://www.w3.org/2000/svg" width="44.322" height="44.322" viewBox="0 0 44.322 44.322">
-                    <g id="Clock" transform="translate(0 28.099) rotate(-60)">
-                        <path id="Path_13" data-name="Path 13" d="M4.867,4.867A14.728,14.728,0,0,1,16.223,0,14.728,14.728,0,0,1,27.579,4.867a14.728,14.728,0,0,1,4.867,11.356,14.728,14.728,0,0,1-4.867,11.356,14.728,14.728,0,0,1-11.356,4.867A14.728,14.728,0,0,1,4.867,27.579C1.893,24.335,0,20.82,0,16.223A14.728,14.728,0,0,1,4.867,4.867ZM23.524,23.524l1.893-1.893-6.76-6.76L16.223,4.056h-2.7V16.223a2.455,2.455,0,0,0,.811,1.893.944.944,0,0,0,.541.27Z" transform="translate(0)" fill="#fff" />
-                    </g>
-                </svg>
-                <p>訂位</p>
-            </div>
-            <div class="sp-like">
-                <svg xmlns="http://www.w3.org/2000/svg" width="31.516" height="27.811" viewBox="0 0 31.516 27.811">
-                    <path id="Heart" d="M29.022,2.544a8.468,8.468,0,0,0-12.053,0L15.783,3.73,14.6,2.544A8.523,8.523,0,0,0,2.544,14.6L15.783,27.836,29.022,14.6a8.468,8.468,0,0,0,0-12.053" transform="translate(-0.025 -0.025)" fill="#fff" fill-rule="evenodd" />
-                </svg>
-                <p>收藏</p>
-            </div>
-            <div class="sp-up">
-                <svg xmlns="http://www.w3.org/2000/svg" width="27.868" height="27.868" viewBox="0 0 27.868 27.868">
-                    <path id="Path_10" data-name="Path 10" d="M13.934,0,11.4,2.533l9.591,9.591H0v3.619H20.992L11.4,25.335l2.533,2.533L27.868,13.934Z" transform="translate(0 27.868) rotate(-90)" fill="#fff" />
-                </svg>
-                <p>往上</p>
-            </div>
+    <h2>CLICK</h2>
+    <div class="form-wrapper">
+        <div class="sp-time">
+            <svg xmlns="http://www.w3.org/2000/svg" width="44.322" height="44.322" viewBox="0 0 44.322 44.322">
+                <g id="Clock" transform="translate(0 28.099) rotate(-60)">
+                    <path id="Path_13" data-name="Path 13" d="M4.867,4.867A14.728,14.728,0,0,1,16.223,0,14.728,14.728,0,0,1,27.579,4.867a14.728,14.728,0,0,1,4.867,11.356,14.728,14.728,0,0,1-4.867,11.356,14.728,14.728,0,0,1-11.356,4.867A14.728,14.728,0,0,1,4.867,27.579C1.893,24.335,0,20.82,0,16.223A14.728,14.728,0,0,1,4.867,4.867ZM23.524,23.524l1.893-1.893-6.76-6.76L16.223,4.056h-2.7V16.223a2.455,2.455,0,0,0,.811,1.893.944.944,0,0,0,.541.27Z" transform="translate(0)" fill="#fff" />
+                </g>
+            </svg>
+            <p>訂位</p>
         </div>
+        <div class="sp-like">
+            <svg xmlns="http://www.w3.org/2000/svg" width="31.516" height="27.811" viewBox="0 0 31.516 27.811">
+                <path id="Heart" d="M29.022,2.544a8.468,8.468,0,0,0-12.053,0L15.783,3.73,14.6,2.544A8.523,8.523,0,0,0,2.544,14.6L15.783,27.836,29.022,14.6a8.468,8.468,0,0,0,0-12.053" transform="translate(-0.025 -0.025)" fill="#fff" fill-rule="evenodd" />
+            </svg>
+            <p>收藏</p>
+        </div>
+        <div class="sp-up">
+            <svg xmlns="http://www.w3.org/2000/svg" width="27.868" height="27.868" viewBox="0 0 27.868 27.868">
+                <path id="Path_10" data-name="Path 10" d="M13.934,0,11.4,2.533l9.591,9.591H0v3.619H20.992L11.4,25.335l2.533,2.533L27.868,13.934Z" transform="translate(0 27.868) rotate(-90)" fill="#fff" />
+            </svg>
+            <p>往上</p>
+        </div>
+    </div>
     </div>
     <div class="companypagbox"></div>
 
@@ -320,11 +358,52 @@
 
         const field = document.querySelector('input[name=avatar]');
 
-        field.addEventListener('change', function() {
-            const fd = new FormData(document.titlepc);
+        const field2 = document.querySelector('.bigpc2');
+
+        // field.addEventListener('change', function() {
+        //     const fd = new FormData(document.titlepc);
+        //     fetch('upload-single-avatar.php', {
+        //             method: 'POST',
+        //             body: fd
+        //         })
+        //         .then(r => r.json())
+        //         .then(obj => {
+        //             console.log(obj);
+        //             document.querySelector('#inputbigimg').src = obj.img;
+        //         })
+        // });
+
+        function previewFile() {
+            const preview = document.querySelector('#inputbigimg');
+            const file = document.querySelector('.inputbigpc').files[0];
+            const reader = new FileReader();
 
 
-        });
+            reader.addEventListener("load", function() {
+                // convert image file to base64 string
+                preview.src = reader.result;
+            }, false);
+
+            if (file) {
+                reader.readAsDataURL(file);
+            }
+        }
+
+        function previewFile2() {
+            const preview = document.querySelector('#inputbigimg2');
+            const file = document.querySelector('.bigpc2').files[0];
+            const reader = new FileReader();
+
+
+            reader.addEventListener("load", function() {
+                // convert image file to base64 string
+                preview.src = reader.result;
+            }, false);
+
+            if (file) {
+                reader.readAsDataURL(file);
+            }
+        }
     </script>
 </body>
 
