ad220fc59c84b8cf82f42d4c0b666a5d4ef0d728
weiweibabo <weiweibabo@gmail.com>
2020-12-11 17:21:45 +0800

    改index檔名

--- E/pages/map2.php
+++ E/pages/map2.php
@@ -55,8 +55,8 @@ $rows = $stmt->fetchAll();
         <div class="search-all">
           <?php foreach ($rows as $r) : ?>
             <div class="search">
-              <a href="product-detail.php?sid=<?= $r['sid'] ?>" >
-                <div class="pic">
+              <a href="product-detail.php?sid=<?= $r['sid'] ?>">
+                <div class="pic web">
                   <img src="<?= WEB_ROOT ?>data/img/<?= $r['sid'] ?>.jpg" alt="" />
                 </div>
 
